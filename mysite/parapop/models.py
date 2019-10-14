02534d89db697448a82d74d7fae5dde146d43250
AlexDmin <alexd555@hotmail.com>
2019-10-14 10:08:33 +0200

    Product post model and migration created.

--- mysite/parapop/models.py
+++ mysite/parapop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class ProductPost(models.Model):
+	title = models.CharField(max_length = 30)
+	description = models.TextField()
+	price = models.FloatField()
+	date_posted = models.DateTimeField(auto_now_add = True)
+	author = models.ForeignKey(User, on_delete= models.CASCADE)
+	productPic = models.ImageField(default = 'defaultProduct.jpg', upload_to = 'product_pics')
+
+	def __str__(self):
+		return self.title
